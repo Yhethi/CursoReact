559f85f8f0ca2d3bbf0b93ce50cb35e823726e8a
Yhethi <eloyjosebonillag@gmail.com>
2023-03-08 16:40:37 -0400

    11-async-await

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,15 +1,31 @@
-const apiKey = "dnccLxB3UTRK7Rk88Uel1rPGMZ5b0nK7";
-// https://api.giphy.com/v1/gifs/random?api_key=dnccLxB3UTRK7Rk88Uel1rPGMZ5b0nK7
+// const apiKey = "dnccLxB3UTRK7Rk88Uel1rPGMZ5b0nK7";
+// // https://api.giphy.com/v1/gifs/random?api_key=dnccLxB3UTRK7Rk88Uel1rPGMZ5b0nK7
 
-const imagen = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+// const imagen = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
 
-imagen
-  .then((data) => data.json())
-  .then(({ data }) => {
-    console.log(data.images.original.url);
-    const {url} = data.images.original;
-    const img = document.createElement('img');
-    img.src = url;
-    document.body.append(img);
-  })
-  .catch(console.warn);
+// imagen
+//   .then((data) => data.json())
+//   .then(({ data }) => {
+//     console.log(data.images.original.url);
+//     const {url} = data.images.original;
+//     const img = document.createElement('img');
+//     img.src = url;
+//     document.body.append(img);
+//   })
+//   .catch(console.warn);
+
+const getImagen = async () => {
+  const apiKey = "dnccLxB3UTRK7Rk88Uel1rPGMZ5b0nK7";
+  const resp = await fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+  );
+  const data = await resp.json();
+  const { data: inf } = data;
+  const { url } = inf.images.original;
+  const img = document.createElement("img");
+  img.src = url;
+  document.body.append(img);
+  console.log(inf);
+};
+
+getImagen();
